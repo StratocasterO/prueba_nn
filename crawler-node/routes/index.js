b625c66b25798c2326edfa0feb67519e93f0874b
stratocastero <omarolmedoferrer@gmail.com>
2020-02-09 01:16:56 +0100

    iterative process for different depths added (to be fixed)

--- crawler-node/routes/index.js
+++ crawler-node/routes/index.js
@@ -3,40 +3,93 @@ var request = require('request');
 var cheerio = require('cheerio');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
 // get request with the starting url to beging the crawl
-router.get('/crawl', function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*'); 
-	
-	// array for saving the links
-	links = [];
-	
-	crawl(req.query.url,1);
-	
-	function crawl(url, depth) {
-		request(url, (error, response, html) => {
+router.get('/crawl', function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-			// if the url responds, extracts the links
+	// array for saving the links
+	links = ["Level 1"];
+
+	crawl(req.query.url, req.query.depth);
+
+	function crawl(url, depth) {
+		iteration = 1;
+
+		crawled = [];
+
+		request(url, (error, response, html) => {
+			// if the url is valid, extracts the links
 			if (!error && response.statusCode == 200) {
-				const $ = cheerio.load(html);
-				
-				$('a').each((i, item) => {
-					const link = $(item).attr('href');
-					
-					links.push(link);
-				});
-				
-				res.send({"content": links});
-			
+				process_link(url,html);
+
 			// if not, returns an error
-			} else if (error){
-				res.send({"content": "There was an error with the url"});
+			} else if (error) {
+				res.send({ "content": "There was an error with the url" });
 			}
+
+			// save the url as crawled
+			crawled.push(url);
+
+			// ends the crawling at selected depth
+			if (iteration = depth) {
+				res.send({ "content": links });
+			} else {
+				iteration += 1;
+
+				links.push("Level " + iteration);
+
+				links.forEach(l => {
+					if (crawled.includes(l)) {
+						// do nothing
+					} else {
+						if (l.includes("http")) {
+							request(l, (error, response, html2) => {
+								// if the url responds, extracts the links
+								if (!error && response.statusCode == 200) {
+									process_link(l,html2);
+
+									// if not, returns an error
+								} else if (error) {
+									res.send({ "content": "There was an error with the url" });
+								}
+
+								crawled.push(l);
+							})
+						};
+					};
+				});
+			}
+
+			function process_link(url,htm) {
+				const $ = cheerio.load(htm);
+
+				// finds the links in the html
+				$('a').each((i, item) => {
+					link = $(item).attr('href');
+
+					// filters the recieved link
+					if (link.substring(0, 1) == '/' && link.substring(1, 2) != '/') {
+						link = url + link;
+						links.push(link);
+					} else if (link.substring(0, 2) == '//') {
+						link = 'http:' + link;
+						links.push(link);
+					} else if (link.substring(0, 1) == '#') {
+						// do nothing
+					} else {
+						// do nothing
+					}
+				});
+
+				// marks the end of a section
+				links.push("-----");
+			};
 		});
-	}
+	};
 });
 
 module.exports = router;
